d9fd2636b6e8d684f45181038c29757f46e7ede8
Nadinda Kalila <nadinda.kalila@ui.ac.id>
2024-12-23 07:39:34 +0700

    Save theme to AsyncStorage

--- context/ThemeContext.js
+++ context/ThemeContext.js
@@ -1,16 +1,36 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import Themes from '../components/Themes';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
     const [themeName, setThemeName] = useState('greenForest');
 
-    const setTheme = (newTheme) => {
+    useEffect(() => {
+        const loadTheme = async () => {
+            try {
+                const savedTheme = await AsyncStorage.getItem("themeName");
+                if (savedTheme && Themes[savedTheme]) {
+                    setThemeName(savedTheme);
+                } 
+            } catch (error) {
+                console.error("Error loading theme", error);
+            }
+        };
+        loadTheme();
+    }, []);
+
+    const setTheme = async (newTheme) => {
         if (Themes[newTheme]) {
             setThemeName(newTheme);
+            try {
+                await AsyncStorage.setItem("themeName", newTheme);
+            } catch (error) {
+                console.error("Error saving theme", error);
+            }
         }
-    }
+    };
 
     const theme = Themes[themeName];
 
